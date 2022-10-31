740e18f40975e731eba399db609350192e3b7453
jervw <vuolajere@gmail.com>
2022-10-31 06:40:51 +0200

    working query

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,41 @@
+use anyhow::Result;
+use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
+use reqwest::blocking::Client;
 
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/schema.gql",
+    query_path = "src/query.gql",
+    response_derives = "Debug"
+)]
+struct Query;
 
+type Date = String;
+
+const ENDPOINT: &str = "https://api.github.com/graphql";
+
+pub fn post_query(user_name: String) -> Result<(), anyhow::Error> {
+    let github_token = "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";
+
+    let vars = query::Variables { user_name };
+
+    let client = Client::builder()
+        .user_agent("grapql-rust/0.11.0")
+        .default_headers(
+            std::iter::once((
+                reqwest::header::AUTHORIZATION,
+                reqwest::header::HeaderValue::from_str(&format!("Bearer {}", github_token))
+                    .unwrap(),
+            ))
+            .collect(),
+        )
+        .build()?;
+
+    let response_body = post_graphql::<Query, _>(&client, ENDPOINT, vars)?;
+
+    let response_data: query::ResponseData = response_body.data.expect("missing response data");
+
+    println!("{:#?}", response_data.user);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use dono::*;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-
+    
     // handle arguments
     if args.len() > 1 {
         let query = &args[1];
         match &query[..] {
-            query => (),
+            query => post_query(query.to_string()).unwrap(),
         }
     }
 }
