a212d83c3e7712a86c08db9e69aa7fd1558699e3
jervw <vuolajere@gmail.com>
2023-01-04 07:54:04 +0200

    use clap for argument parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use anyhow::{anyhow, Error, Result};
 use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
 use reqwest::blocking::Client;
@@ -12,14 +13,15 @@ struct Query;
 
 type Date = String;
 
-struct Contribution {
-    date: String,
-    color: String,
+pub struct Contribution {
+    pub date: String,
+    pub count: i64,
+    pub color: String,
 }
 
 const ENDPOINT: &str = "https://api.github.com/graphql";
 
-pub fn post_query(user_name: String) -> Result<query::ResponseData, Error> {
+pub fn post_query(user_name: String) -> Result<query::QueryUser, Error> {
     dotenv::dotenv().ok();
     let github_token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN must be set");
 
@@ -39,21 +41,25 @@ pub fn post_query(user_name: String) -> Result<query::ResponseData, Error> {
 
     let response_body = post_graphql::<Query, _>(&client, ENDPOINT, vars)?;
 
-    response_body
-        .data
-        .ok_or_else(|| anyhow!("no data in response"))
+    let user_data = match response_body.data {
+        Some(data) => data.user.ok_or(anyhow!("user not found"))?,
+        None => return Err(anyhow!("unable to retrieve data")),
+    };
+
+    Ok(user_data)
 }
 
-
-pub fn parse_contributions(response: query::ResponseData) -> Vec<Contribution> {
-    let mut contributions = Vec::new();
-    for week in response.user.contributions_collection.contribution_calendar.weeks {
-        for day in week.contribution_days {
-            contributions.push(Contribution {
-                date: day.date.to_string(),
-                color: day.color.to_string(),
-            });
-        }
-    }
-    contributions
+pub fn parse_contributions(response: query::QueryUser) -> Vec<Contribution> {
+    response
+        .contributions_collection
+        .contribution_calendar
+        .weeks
+        .into_iter()
+        .flat_map(|week| week.contribution_days)
+        .map(|day| Contribution {
+            date: day.date,
+            count: day.contribution_count,
+            color: day.color,
+        })
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-use std::process;
-
+use clap::Parser;
 use dono::*;
 
+#[derive(Parser)]
+#[clap(
+    name = "dono",
+    about = "A CLI tool to show your GitHub contributions",
+    version = "0.1.0"
+)]
+struct Args {
+    /// GitHub user name
+    #[clap(name = "USER_NAME")]
+    user_name: String,
+}
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    
-    // handle arguments
-    if args.len() > 1 {
-        let query = &args[1];
-        match &query[..] {
-            query => post_query(query.to_string()).unwrap(),
+    let args = Args::parse();
+
+    match post_query(args.user_name) {
+        Ok(response) => {
+            let contributions = parse_contributions(response);
+            for contribution in contributions {
+                println!("{} {}", contribution.date, contribution.color);
+            }
         }
+        Err(e) => println!("Error: {}", e),
     }
 }
